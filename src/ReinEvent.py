30c3acd11e48652ec73c18873480d6f20ab0e8ce
Ren <delosreyes.ren@gmail.com>
2014-11-21 14:14:03 -0500

    Took out without block.

--- src/ReinEvent.py
+++ src/ReinEvent.py
@@ -83,135 +83,126 @@ def parse_events(FM_Dict):
     for entry in calendar.entries:
         
         #fix any unicode errors or what can't translate well
-        description = entry.description.split("<br />", 1) #split the description into a part
+        description = entry.description.split("<br />", 2) #split the description into two parts
         entry.title.decode("utf-8", "strict").encode("utf-8", "ignore") #in case there's a title in a foreign language
         
-        #check if the event is within the events list already
-        without = True
-        for event in events:
-            #does it have the title (entry.title) and location (description[0]) as the same thing?
-            if (event.name == entry.title) and (event.location == description[0]):
-                without = False
-                
-        if without: #if event isn't already within the events list, add it to the events list
+        #remove quotation marks in title because it creates conflict when putting the name in CartoDB table
+        if ("\'" in entry.title) or ("\"" in entry.title):
+            title = "" 
+            for letter in entry.title: #go through letter by letter in the title to check if it's a quotation mark
+                if (letter != "'") and (letter != "\""): #if not a quotation mark, add it to the title
+                    title += letter
+            name = title
+        else: #if quotation marks doesn't exist in the title, go ahead and grab it
+            name = entry.title
             
-            #remove quotation marks in title because it creates conflict when putting the name in CartoDB table
-            if ("\'" in entry.title) or ("\"" in entry.title):
-                title = "" 
-                for letter in entry.title: #go through letter by letter in the title to check if it's a quotation mark
-                    if (letter != "'") and (letter != "\""): #if not a quotation mark, add it to the title
-                        title += letter
-                name = title
-            else: #if quotation marks doesn't exist in the title, go ahead and grab it
-                name = entry.title
+        '''
+        Sometimes there's an exception where an event doesn't have a location, thus no FMCode, and has the date and time of the event in the
+        description. 
+        '''
+        #format location
+        location = description[0] 
+        
+        #format fmcode
+        building_name = ""
+        possibleBuildings = []
+        keywords = location.split() # split the location name so we can isolate the building name
+        
+        print "EVENT NAME: " + name
+        total_events += 1
+        # loops through all building names to determine which one the event is located at        
+        for building in FM_Dict.keys():
+            if keywords[0] in building:
+                possibleBuildings.append(building)       
+                #print building     
+                print "FIRST WORD OF EVENT LOCATION: " + keywords[0]
+                print "FIRST WORD OF EVENT LOCATION MATCHES FIRST WORD OF: "
+                print possibleBuildings
                 
-            '''
-            Sometimes there's an exception where an event doesn't have a location, thus no FMCode, and has the date and time of the event in the
-            description. 
-            '''
-            #format location
-            location = description[0] 
+        ''' EXCEPTIONS TO WORK ON
             
-            #format fmcode
-            building_name = ""
-            possibleBuildings = []
-            keywords = location.split() # split the location name so we can isolate the building name
+            NO LOCATION
             
-            print "EVENT NAME: " + name
-            total_events += 1
-            # loops through all building names to determine which one the event is located at        
-            for building in FM_Dict.keys():
-                if keywords[0] in building:
-                    possibleBuildings.append(building)       
-                    #print building     
-                    print "FIRST WORD OF EVENT LOCATION: " + keywords[0]
-                    print "FIRST WORD OF EVENT LOCATION MATCHES FIRST WORD OF: "
-                    print possibleBuildings
-                    
-            ''' EXCEPTIONS TO WORK ON
-                
-                NO LOCATION
-                
-                IF IT CAN'T FIND IT THEN JUST MOVE ON (KeyError)
-                
-                LOCATIONS THAT WORK AREN'T IN THE BUILDINGS TABLE (EX. THE QUAD)
-                //IF BUILDINGS TABLE WAS ACCORDING TO WHERE YOU COULD BOOK EVENTS
-                
-                REMIND TO GET UPDATED LIST EVERY YEAR
-                
-                WHY IS IT PRINTING POSSIBLE BUILDINGS TWICE? DOESN'T REALLY MATTER
-                
-                NEEDS TO ACCOMODATE FOR SAME EVENT ON DIFFERENT DATES
-                
-                WILL EVENTUALLY ACCOUNT FOR EXCEPTIONS INSTEAD OF HAVING SPECIFIC ELIFS
-                
-                THE RSS FEED ALLOWS FOR ABOUT 35 EVENTS
-                
-                DOES NOT FIND TV STUDIO
-                CHAPIN HOUSE LAWN IS NOT ACCOUNTED FOR. NEED TO FIX HOW MANY KEYWORDS THE CODE CHECKS! MORE THAN 2!
-                NEILSON BROWSING ROOM DEFAULTS TO NEILSON LIBRARY
-            ''' 
-                  
-            try:
-                if len(possibleBuildings) > 1:
-                    for possibility in possibleBuildings:
-                        if keywords[1] in possibility:
-                            building_name = possibility    
-                            print "CHECKED TWO FIRST WORDS AND GOT A MATCH. FOR NOW WE WILL USE THE MATCH BUT WILL HAVE TO ACCOUNT FOR MORE LATER"
-                elif len(possibleBuildings) == 1:
-                    building_name = possibleBuildings[0]
-                    print "ONLY ONE POSSIBLE BUILDING: " + building_name
-                elif (keywords[0] == "Sweeney") or (keywords[0] == "Earle"):
-                    building_name = "Sage Hall"
-                elif (keywords[0] == "Lewis") or (keywords[0] == "Weinstein"):
-                    building_name = "Wright Hall"
-                elif (keywords[0] == "Hallie") or (keywords[0] == "Theatre") or (keywords[0] == "Formerly"):
-                    building_name = "Mendenhall Center for Performing Arts"
-                elif (keywords[0] == "BFAC") or (keywords[0] == "Hillyer") or (keywords[0] == "Graham"):
-                    building_name = "Fine Arts Center"
-                elif (keywords[0] == "Quad"):
-                    building_name = "Wilson House"
-                #only one code so get it
-                else:
-                    # IF THE LIST IS BLANK OR THERE IS A DATE INSTEAD OF AN EVENT
-                    print "NO POSSIBLE EVENT LOCATIONS"
+            IF IT CAN'T FIND IT THEN JUST MOVE ON (KeyError)
+            
+            LOCATIONS THAT WORK AREN'T IN THE BUILDINGS TABLE (EX. THE QUAD)
+            //IF BUILDINGS TABLE WAS ACCORDING TO WHERE YOU COULD BOOK EVENTS
+            
+            REMIND TO GET UPDATED LIST EVERY YEAR
+            
+            WHY IS IT PRINTING POSSIBLE BUILDINGS TWICE? DOESN'T REALLY MATTER
+            
+            NEEDS TO ACCOMODATE FOR SAME EVENT ON DIFFERENT DATES
+            
+            WILL EVENTUALLY ACCOUNT FOR EXCEPTIONS INSTEAD OF HAVING SPECIFIC ELIFS
+            
+            THE RSS FEED ALLOWS FOR ABOUT 35 EVENTS
+            
+            DOES NOT FIND TV STUDIO
+            CHAPIN HOUSE LAWN IS NOT ACCOUNTED FOR. NEED TO FIX HOW MANY KEYWORDS THE CODE CHECKS! MORE THAN 2!
+            NEILSON BROWSING ROOM DEFAULTS TO NEILSON LIBRARY
+        ''' 
+              
+        try:
+            if len(possibleBuildings) > 1:
+                for possibility in possibleBuildings:
+                    if keywords[1] in possibility:
+                        building_name = possibility    
+                        print "CHECKED TWO FIRST WORDS AND GOT A MATCH. FOR NOW WE WILL USE THE MATCH BUT WILL HAVE TO ACCOUNT FOR MORE LATER"
+            elif len(possibleBuildings) == 1:
+                building_name = possibleBuildings[0]
+                print "ONLY ONE POSSIBLE BUILDING: " + building_name
+            elif (keywords[0] == "Sweeney") or (keywords[0] == "Earle"):
+                building_name = "Sage Hall"
+            elif (keywords[0] == "Lewis") or (keywords[0] == "Weinstein"):
+                building_name = "Wright Hall"
+            elif (keywords[0] == "Hallie") or (keywords[0] == "Theatre") or (keywords[0] == "Formerly"):
+                building_name = "Mendenhall Center for Performing Arts"
+            elif (keywords[0] == "BFAC") or (keywords[0] == "Hillyer") or (keywords[0] == "Graham"):
+                building_name = "Fine Arts Center"
+            elif (keywords[0] == "Quad"):
+                building_name = "Wilson House"
+            #only one code so get it
+            else:
+                # IF THE LIST IS BLANK OR THERE IS A DATE INSTEAD OF AN EVENT
+                print "NO POSSIBLE EVENT LOCATIONS"
 
-                fmcode = FM_Dict[building_name] #look up fmcode using the building name and FMCodedictionary
-                                
-                '''
-                Possibly grab date and time from <pubdate> field rather than description[1]
-                '''
-                #format date
-                date = description[1].split(",") #split string whenever it encounters a comma
-                date_time = ",".join(date[:2]), ",".join(date[2:]) #only split until the second comma
-                date = date_time[0]
-                    
-                '''
-                date_time = entry.pubDate.split() #split pubDate's string into four parts
-                date = date_time[0] + date_time[1] + date_time[2] #join the first three parts
-                http://www.quora.com/How-can-I-convert-a-GMT-time-zone-into-local-time-in-Python
-                http://stackoverflow.com/questions/6288892/convert-datetime-format
-                '''
-                #format time
-                time = date_time[1].split()
-                times = time[1].split("&nbsp;&ndash;&nbsp;") #get rid of the character " - " and split the string there
-                time = times[0] + " - " + times[1] #concatenate strings with the time    
-    
-                #create an Event object
-                e = Event(name, location, fmcode, time, date)
-                events.append(e)
-                events_added+=1
-                print e.name + " has been added to the list of events"
-                print e.name + "'s" + " loca: " + building_name
-                print e.name + "'s" + " time: " + e.time
-                print e.name + "'s" + " date: " + e.date
-                print
-                #print e.name
-            except KeyError:    
-                # can't match the event, just keep going                   
-                print "could not resolve event: " + name
-                print
-                events_dropped+=1
+            fmcode = FM_Dict[building_name] #look up fmcode using the building name and FMCodedictionary
+                            
+            '''
+            Possibly grab date and time from <pubdate> field rather than description[1]
+            '''
+            #format date
+            date = description[1].split(",") #split string whenever it encounters a comma
+            date_time = ",".join(date[:2]), ",".join(date[2:]) #only split until the second comma
+            date = date_time[0]
+                
+            '''
+            date_time = entry.pubDate.split() #split pubDate's string into four parts
+            date = date_time[0] + date_time[1] + date_time[2] #join the first three parts
+            http://www.quora.com/How-can-I-convert-a-GMT-time-zone-into-local-time-in-Python
+            http://stackoverflow.com/questions/6288892/convert-datetime-format
+            '''
+            #format time
+            time = date_time[1].split()
+            times = time[1].split("&nbsp;&ndash;&nbsp;") #get rid of the character " - " and split the string there
+            time = times[0] + " - " + times[1] #concatenate strings with the time    
+
+            #create an Event object
+            e = Event(name, location, fmcode, time, date)
+            events.append(e)
+            events_added+=1
+            print e.name + " has been added to the list of events"
+            print e.name + "'s" + " loca: " + building_name
+            print e.name + "'s" + " time: " + e.time
+            print e.name + "'s" + " date: " + e.date
+            print
+            #print e.name
+        except KeyError:    
+            # can't match the event, just keep going                   
+            print "could not resolve event: " + name
+            print
+            events_dropped+=1
                 
     print
     print "Finished parsing..."            
